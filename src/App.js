fddb9cf5ce198c5531e98703f64396151e9d562f
Hossein Fakhari <fakhari.hossein@gmail.com>
2020-08-28 18:14:57 +0430

    set user redirect action to avoid login page if user is already logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setCurrentUser} from "./redux/user/user.actions";
 
@@ -47,16 +47,21 @@ class App extends React.Component {
                 <Switch>
                     <Route exact={true} path='/' component={HomePage}/>
                     <Route exact={true} path='/shop' component={ShopPage}/>
-                    <Route exact={true} path='/signin' component={SignInAndSignUpPage}/>
+                    <Route exact={true} path='/signin'
+                           render={()=> this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
                 </Switch>
             </div>
         )
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
