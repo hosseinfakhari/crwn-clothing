1fae40f65da8e56e552e15304d7f301f848500c0
Hossein Fakhari <fakhari.hossein@gmail.com>
2020-08-28 17:56:46 +0430

    adding redux to project and moving user object from state to redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {setCurrentUser} from "./redux/user/user.actions";
+
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -10,29 +13,23 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            currentUser: null
-        }
-    }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+        const {setCurrentUser} = this.props
+
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = createUserProfileDocument(userAuth);
                 (await userRef).onSnapshot(snapshot => {
-                    this.setState({
-                        currentUser: {
-                            id: snapshot.id,
-                            ...snapshot.data()
-                        }
-                    })
+                    setCurrentUser({
+                        id: snapshot.id,
+                        ...snapshot.data()
+                    });
                 })
             } else {
-                this.setState({currentUser: userAuth})
+                setCurrentUser(userAuth)
             }
 
 
@@ -46,7 +43,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser}/>
+                <Header/>
                 <Switch>
                     <Route exact={true} path='/' component={HomePage}/>
                     <Route exact={true} path='/shop' component={ShopPage}/>
@@ -57,4 +54,9 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './header.style.scss';
 import {Link} from "react-router-dom";
+import {connect} from 'react-redux';
+
 
 import {ReactComponent as Logo} from '../../assests/crown.svg'
 
@@ -25,4 +27,8 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
