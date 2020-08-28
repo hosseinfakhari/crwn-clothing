e43d3468a33a88800e917d61da8a804fd62dc3c2
Hossein Fakhari <fakhari.hossein@gmail.com>
2020-08-28 22:46:25 +0430

    using reslect for getting part of data from redux to avoid re rendering

--- src/components/cart-dropdown/cart-dropdown.componenet.jsx
+++ src/components/cart-dropdown/cart-dropdown.componenet.jsx
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 
 import './cart-dropdown.style.scss';
 
+import {selectCartItems} from "../../redux/cart/cart.selectors";
+
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 
@@ -17,8 +19,8 @@ const CartDropDown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+     cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown);
