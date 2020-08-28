b2bb9bf91f177fa3100a471ea246945b719d9994
Hossein Fakhari <fakhari.hossein@gmail.com>
2020-08-28 19:30:18 +0430

    adding cart dropdown show/hide functionality with redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import './header.style.scss';
 import {Link} from "react-router-dom";
 import {connect} from 'react-redux';
-
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.componenet";
 
 import {ReactComponent as Logo} from '../../assests/crown.svg'
 
 import {auth} from "../../firebase/firebase.utils";
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -16,19 +17,22 @@ const Header = ({currentUser}) => (
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/shop'>CONTACT</Link>
-
             {
                 currentUser ?
-                    <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                     :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
+
         </div>
+        {hidden ? null : <CartDropDown/>}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
